bcabdd3240fbad7f7a9ddf4fe56505df2e912410
10Hyuna <kimha105@naver.com>
2023-06-19 04:20:37 +0900

    [solved] class 4 - 10

--- 브론즈/class4/src/Main.java
+++ 브론즈/class4/src/Main.java
@@ -8,35 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        int n = Integer.parseInt(br.readLine());
+
+        int[] score = new int[n];
         String line = br.readLine();
         st = new StringTokenizer(line, " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
 
-        int i, j, tmp;
-
-        int[] bucket = new int[n];
-        for(int a = 0; a < n; a++){
-            bucket[a] = a + 1;
+        for(int i = 0; i < n; i++){
+            score[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int a = 0; a < m; a++){
-            line = br.readLine();
-            st = new StringTokenizer(line, " ");
-            i = Integer.parseInt(st.nextToken()) - 1;
-            j = Integer.parseInt(st.nextToken()) - 1;
+        int max = score[0];
 
-            while(i < j){
-                tmp = bucket[i];
-                bucket[i] = bucket[j];
-                bucket[j] = tmp;
-                i++;
-                j--;
+        for(int i = 0; i < n; i++){
+            if(score[i] > max){
+                max = score[i];
             }
         }
 
-        for(int a = 0; a < n; a++){
-            System.out.printf("%d ", bucket[a]);
+        double total = 0;
+
+        for(int i = 0; i < n; i++){
+            total += (score[i] / (double)max * 100);
         }
+
+        System.out.print(total / n);
     }
 }
