800dd9b4abe2feb2f56388c1ae38deb7c32396e0
10Hyuna <kimha105@naver.com>
2023-06-19 16:59:11 +0900

    [solved] class 5 - 3

--- 브론즈/class5/src/Main.java
+++ 브론즈/class5/src/Main.java
@@ -5,8 +5,14 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        String line;
+        String foundText;
 
-        System.out.println(line.length());
+        for(int i = 0; i < n; i++){
+            line = br.readLine();
+            foundText = String.format("%c%c", line.charAt(0), line.charAt(line.length() - 1));
+            System.out.println(foundText);
+        }
     }
 }
