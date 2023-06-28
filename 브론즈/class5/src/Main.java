c5768295bad7329a9bd024f5d77c787ef8e87091
10Hyuna <kimha105@naver.com>
2023-06-28 10:23:46 +0900

    [solved] quiz) 2675

--- 브론즈/class5/src/Main.java
+++ 브론즈/class5/src/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int foundIndex;
-        String foundAlphabet;
+        int count = Integer.parseInt(br.readLine());
 
-        String line = br.readLine();
+        String line;
+        int refeatCount;
+        String refeatText;
 
-        for(int i = 0; i < alphabet.length(); i++){
-            foundIndex = -1;
-            foundAlphabet = alphabet.substring(i, i + 1);
-            for(int j = 0; j < line.length(); j++){
-                if(foundAlphabet.equals(line.substring(j, j + 1))){
-                    foundIndex = j;
-                    break;
+        for(int i = 0; i < count; i++){
+            line = br.readLine();
+            refeatCount = Integer.parseInt(line.substring(0, 1));
+            refeatText = line.substring(2);
+            for(int j = 0; j < refeatText.length(); j++){
+                for(int k = 0; k < refeatCount; k++){
+                    System.out.printf("%s", refeatText.substring(j, j + 1));
                 }
             }
-            System.out.printf("%d ", foundIndex);
+            System.out.printf("\n");
         }
     }
 }
