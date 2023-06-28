c62b89a7c67de5b3242e341d9ec864a176055f15
10Hyuna <kimha105@naver.com>
2023-06-28 10:33:25 +0900

    [solved] quiz) 1152

--- 브론즈/class5/src/Main.java
+++ 브론즈/class5/src/Main.java
@@ -5,23 +5,26 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Boolean isText = false;
+        int sum = 0;
 
-        int count = Integer.parseInt(br.readLine());
+        String line = br.readLine();
 
-        String line;
-        int refeatCount;
-        String refeatText;
-
-        for(int i = 0; i < count; i++){
-            line = br.readLine();
-            refeatCount = Integer.parseInt(line.substring(0, 1));
-            refeatText = line.substring(2);
-            for(int j = 0; j < refeatText.length(); j++){
-                for(int k = 0; k < refeatCount; k++){
-                    System.out.printf("%s", refeatText.substring(j, j + 1));
+        for(int i = 0; i < line.length(); i++){
+            if(line.substring(i, i + 1).equals(" ")){
+                if(isText){
+                    sum++;
                 }
+                isText = false;
             }
-            System.out.printf("\n");
+            else{
+                isText = true;
+            }
         }
+        if(isText){
+            sum++;
+        }
+
+        System.out.printf("%d", sum);
     }
 }
