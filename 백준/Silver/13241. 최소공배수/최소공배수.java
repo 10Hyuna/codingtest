f56981e3abe064dd4ca4597cae69f06e9249172a
10Hyuna <129165742+10Hyuna@users.noreply.github.com>
2023-08-31 22:32:59 +0900

    [Silver V] Title: 최소공배수, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long num1 = Long.parseLong(st.nextToken());
+        long num2 = Long.parseLong(st.nextToken());
+
+        System.out.println(num1 * num2 / gcd(num1, num2));
+    }
+
+    private static long gcd(long num1, long num2) {
+        if (num2 == 0) {
+            return num1;
+        } else {
+            return gcd(num2, num1 % num2);
+        }
+    }
+}
